a00b3e2291d8e1113eb38bc6ebc51dc40a381912
jxwide <96314993+jxwide@users.noreply.github.com>
2022-01-30 12:46:01 +0300

    master

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+const path = require('path')
+const port = process.env.PORT || 3000
+
+// hbs
+app.set('view engine', 'hbs')
+hbs.registerPartials(path.resolve(__dirname, 'views', 'partials'))
+
+const start = async () => {
+    app.get('/', (req, res) => {
+        res.render('index.hbs')
+    })
+
+    app.get('/test', (req, res) => {
+        res.render('articleslist.hbs')
+    })
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
+
+start()
